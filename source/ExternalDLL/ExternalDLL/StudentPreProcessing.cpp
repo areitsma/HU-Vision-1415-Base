8ea31a8970d472e456821df8464589228831eabd
areitsma <arnout.reitsma@student.hu.nl>
2015-04-26 16:24:54 +0200

    Week 1 klaar

--- source/ExternalDLL/ExternalDLL/StudentPreProcessing.cpp
+++ source/ExternalDLL/ExternalDLL/StudentPreProcessing.cpp
@@ -1,8 +1,17 @@
 #include "StudentPreProcessing.h"
-
-
+#include "IntensityImageStudent.h"
+#include <algorithm>
 IntensityImage * StudentPreProcessing::stepToIntensityImage(const RGBImage &image) const {
-	return nullptr;
+	IntensityImageStudent * IntImage = new IntensityImageStudent{ image.getWidth(), image.getHeight() };
+	for (int i = 0; i < image.getHeight()* image.getWidth(); i++){
+			RGB rgbPix = image.getPixel(i); 
+			//convert RGB values to gray/intensity value
+			//int Gray = (rgbPix.r + rgbPix.g + rgbPix.b) / 3; //Method Average
+			//int Gray = (rgbPix.r * 0.299 + rgbPix.g  * 0.587 + rgbPix.b  * 0.114); //Method Luma
+			int Gray = (std::max({ rgbPix.r, rgbPix.g, rgbPix.b }) + std::min({ rgbPix.r, rgbPix.g, rgbPix.b})) / 2; // Method Desaturation
+			IntImage->setPixel(i, Gray);
+		}
+	return IntImage;
 }
 
 IntensityImage * StudentPreProcessing::stepScaleImage(const IntensityImage &image) const {
